b7c32146ef1e98c9f93faff7d0f9410e91c5ca23
Wu Jianyue <wujianyue000@163.com>
2022-11-22 07:00:11 +0000

    Add python error code for sonar scan

---
+++ sonar_postgreSQL/sonar_python_scan/src/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+#Author:Zhang ShiJie
+import  sys
+
+for i in range(50):
+    if i == 10:
+        print(i)
+    elif i == 20:
+        print(i)
+    elif i == 20:
+        print(i)
+    else:
+        pass
