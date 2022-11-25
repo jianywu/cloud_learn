fde8218e8c39639090490ef280028dc686ba7f53
jianywu <wujianyue000@gmail.com>
2022-11-25 09:51:32 +0800

    update pipeline all

--- jenkins_learn/pipeline/pipeline_all.groovy
+++ jenkins_learn/pipeline/pipeline_all.groovy
@@ -6,16 +6,18 @@ pipeline {
         disableConcurrentBuilds()
     }
     environment {
-        def NODE_1 = "114.115.154.84"
-        def NODE_3 = "114.115.144.163"
+        def NODE_BUILD = "114.115.154.84"
+        def NODE_GITLAB = "114.115.144.163"
+        def NODE_HARBOR = "114.115.221.236"
         def APP1_DEPLOY_DIR = '/var/lib/jenkins/workspace/magedu-app1_deploy-20221120'
-        def TEST_GIT_URL = 'git@114.115.144.163:user3/test.git'
-        def WEB_GIT_URL = 'git@114.115.144.163:user3/web_test.git'
-        def SONAR_GIT_URL = 'git@114.115.144.163:user3/sonar_python_scan.git'
+        def TEST_GIT_URL = "git@${env.NODE_GITLAB}:user3/test.git"
+        def WEB_GIT_URL = "git@${env.NODE_GITLAB}:user3/web_test.git"
+        def SONAR_GIT_URL = "git@${env.NODE_GITLAB}:user3/sonar_python_scan.git"
         def HARBOR_URL = 'harbor.magedu.net'
         def IMAGE_PROJECT = 'myserver'
         def IMAGE_NAME = 'nginx'
         def DATE = sh(script:'date +%F_%H-%M-%S', returnStdout: true).trim()
+        // def GIT_COMMIT_TAG = ""
     }
 
     parameters {
@@ -27,20 +29,26 @@ pipeline {
         stage('code clone') {
             agent { label 'jenkins-node1' }  // not mandatory to configure in stage
             steps {
-                sh "rm -rf '/var/lib/jenkins/workspace/magedu-app1_deploy-20221120';mkdir -p '/var/lib/jenkins/workspace/magedu-app1_deploy-20221120'"
+                echo "Clean up env:"
+                sh "rm -rf '/var/lib/jenkins/workspace/magedu-app1_deploy-20221120'"
+                sh "mkdir -p '/var/lib/jenkins/workspace/magedu-app1_deploy-20221120'"
                 dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120') {
                     script {
+                        echo "start clone code:"
                         if (env.BRANCH == 'main') {
-                            sh "git clone -b main ${env.TEST_GIT_URL}"
-                            sh "git clone -b main ${env.SONAR_GIT_URL}"
-                            sh "git clone -b main ${env.WEB_GIT_URL}"
+                            git branch: 'main', credentialsId: '23e49037-3cb3-4dc6-9af9-fbe6a92eec11', url: "${env.WEB_GIT_URL}"
+                            // sh "git clone -b main ${env.WEB_GIT_URL}"
                         } else if (env.BRANCH == 'develop') {
-                            sh 'git clone -b develop git@114.115.144.163:test/test.git'
-                        } else {
+                            git branch: 'develop', credentialsId: '23e49037-3cb3-4dc6-9af9-fbe6a92eec11', url: "${env.WEB_GIT_URL}"
                             echo 'BRANCH PARAMETER ERROR'
                         }
-                        GIT_COMMIT_TAG = sh 'cd test;git rev-parse --short HEAD'
+                        // Note: GIT_COMMIT_TAG = sh 'git rev-parse --short HEAD' is null.
+                        GIT_COMMIT_TAG = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+                        // Note: ${env.GIT_COMMIT_TAG} is null.
+                        echo "GIT_COMMIT_TAG is ${GIT_COMMIT_TAG}"
                     }
+                    // sh "git clone -b main ${env.TEST_GIT_URL}"
+                    // sh "git clone -b main ${env.SONAR_GIT_URL}"
                 }
             }
         }
@@ -49,7 +57,8 @@ pipeline {
             agent { label 'jenkins-node1' }  // not mandatory to configure in stage
             steps {
                 dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120') {
-                    // some block
+                    sh "git clone -b main ${env.SONAR_GIT_URL}"
+                    echo "start sonar qube scan:"
                     sh '''
                         /apps/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=magedu -Dsonar.projectName=magedu-app1 -Dsonar.projectVersion=1.0  -Dsonar.sources=./sonar_python_scan -Dsonar.language=py -Dsonar.sourceEncoding=UTF-8
                     '''
@@ -60,7 +69,7 @@ pipeline {
         stage('code build') {
             agent { label 'jenkins-node1' }
             steps {
-                dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120/web_test/app1-frontend-files-5.101') {
+                dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120/app1-frontend-files-5.101') {
                     // some block
                     sh 'tar czvf frontend.tar.gz ./index.html ./images'
                 }
@@ -70,40 +79,43 @@ pipeline {
         stage('file sync') {  //SSH Pipeline Steps
             agent { label 'jenkins-node1' }
             steps {
-                dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120/web_test') {
-                    sh "ssh root@${env.NODE_3} 'mkdir -p /opt/ubuntu-dockerfile'"
-                    sh "ssh root@${env.NODE_3} 'docker images'"
-                    sh "ssh root@${env.NODE_3} 'docker ps'"
-                    sh "scp ./app1-frontend-files-5.101/frontend.tar.gz root@${env.NODE_3}:/opt/ubuntu-dockerfile"
-                    sh "scp ./ubuntu-dockerfile-6.201/build-command.sh root@${env.NODE_3}:/opt/ubuntu-dockerfile"
+                dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120') {
+                    sh "ssh root@${env.NODE_HARBOR} 'mkdir -p /opt/ubuntu-dockerfile'"
+                    sh "ssh root@${env.NODE_HARBOR} 'docker images'"
+                    sh "ssh root@${env.NODE_HARBOR} 'docker ps'"
+                    sh "scp ./app1-frontend-files-5.101/frontend.tar.gz root@${env.NODE_HARBOR}:/opt/ubuntu-dockerfile"
+                    sh "scp -r ./ubuntu-dockerfile-6.201/* root@${env.NODE_HARBOR}:/opt/ubuntu-dockerfile"
                 }
             }
         }
 
         stage('image build') {  //SSH Pipeline Steps
-            agent { label 'jenkins-node3' }
+            agent { label 'jenkins-node4' }
             steps {
-                sh "mkdir -p '/var/lib/jenkins/workspace/magedu-app1_deploy-20221120'"
-                dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120') {
-                    sh "cd /opt/ubuntu-dockerfile/ && bash ./build-command.sh ${GIT_COMMIT_TAG}-${DATE}"
-                }
+                // sh "ip a"
+                sh "docker login harbor.magedu.net"
+                // sh "mkdir -p '/var/lib/jenkins/workspace/magedu-app1_deploy-20221120'"
+                // dir('/var/lib/jenkins/workspace/magedu-app1_deploy-20221120') {
+                echo "GIT_COMMIT_TAG in build is ${GIT_COMMIT_TAG}"
+                sh "cd /opt/ubuntu-dockerfile/ && bash ./build-command.sh ${GIT_COMMIT_TAG}-${DATE}"
+                // }
             }
         }
 
         stage('docker-compose image update') {
             steps {
-                agent { label 'jenkins-node1' }
+                // agent { label 'jenkins-node4' }
                 sh """
-                    ssh root@${env.NODE_3} "echo ${DATE} && rm -rf /data/magedu-app1;mkdir -p /data/magedu-app1;cd /data/magedu-app1 && sed -i  's#image: harbor.magedu.net/myserver/nginx:.*#image: harbor.magedu.net/myserver/nginx:${GIT_COMMIT_TAG}-${DATE}#' docker-compose.yml"
+                    ssh root@${env.NODE_BUILD} "echo ${DATE} && rm -rf /data/magedu-app1;mkdir -p /data/magedu-app1;cd /data/magedu-app1 && cp /var/lib/jenkins/workspace/magedu-app1_deploy-20221120/magedu-app1-composefile-6.202/docker-compose.yml /data/magedu-app1 && sed -i  's#image: harbor.magedu.net/myserver/nginx:.*#image: harbor.magedu.net/myserver/nginx:${GIT_COMMIT_TAG}-${DATE}#' docker-compose.yml"
                 """
             }
         }
 
         stage('docker-compose app update') {
             steps {
-                agent { label 'jenkins-node1' }
+                // agent { label 'jenkins-node4' }
                 sh """
-                   ssh root@${env.NODE_3} "echo ${DATE} && cd /data/magedu-app1 && docker-compose pull && docker-compose up -d"
+                   ssh root@${env.NODE_BUILD} "echo ${DATE} && cd /data/magedu-app1 && docker-compose pull && docker-compose up -d"
                 """
             }
         }
@@ -124,4 +136,3 @@ pipeline {
         }
     }
 }
-
