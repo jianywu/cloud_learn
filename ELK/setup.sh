2e32edc4f20e531de0c1ec6e2f81d6b86ba33500
jianywu <wujianyue000@gmail.com>
2022-11-28 20:06:29 +0800

    Update ES configs

--- ELK/setup.sh
+++ ELK/setup.sh
@@ -1,6 +1,9 @@
 set -x
+# Add user and group for ES
 groupadd -g 2888 elasticsearch && useradd -u 2888 -g 2888 -r -m -s /bin/bash elasticsearch
+# mkdir
 mkdir /data/esdata /data/eslogs /apps -pv
+# untar elasticsearch.tgz, softlink to /apps/elasticsearch, change /apps is better
 chown elasticsearch.elasticsearch /data /apps -R
 
 echo "other manual steps see README.md"
