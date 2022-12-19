fea388ed350ab05b9f177e110a94cc0a6026f638
Wu Jianyue <wujianyue000@163.com>
2022-12-19 15:33:46 +0800

    User mimimal servers for lab

--- ceph/ceph_install/common_install.sh
+++ ceph/ceph_install/common_install.sh
@@ -10,25 +10,28 @@ apt install -y apt-transport-https ca-certificates curl software-properties-comm
 apt install -y ceph-base ceph-common ceph-fuse ceph-mds ceph-mon ceph-osd --allow-unauthenticated
 
 # 加key文件
-wget -q -O- 'https://mirrors.tuna.tsinghua.edu.cn/ceph/keys/release.asc' | sudo apt-key add -
+#wget -q -O- 'https://mirrors.tuna.tsinghua.edu.cn/ceph/keys/release.asc' | sudo apt-key add -
 
-# 如果22.04是jammy，而20.04是focal，通过lsb_release -sc查看。
+# 如果22.04是jammy，而20.04是focal，通过lsb_release -sc查看。虚拟机需要，云主机可以不用，一般都默认配置了这个库。
 # echo "deb https://mirrors.tuna.tsinghua.edu.cn/ceph/debian-pacific focal main" >> /etc/apt/sources.list
-echo "deb http://download.ceph.com/debian-jewel/ jammy main" >> /etc/apt/sources.list
-apt update -y
+#echo "deb http://download.ceph.com/debian-jewel/ jammy main" >> /etc/apt/sources.list
+#apt update -y
 
 # 免密钥登录，先在/etc/hosts里加上各主机的解析。Here Document的cat << EOF方式重定向即可。也可以直接echo
+ip1="42.51.17.66"
+ip2="125.124.238.46"
+ip3="114.115.144.163"
 (
 cat << EOF
-$ip ceph-deploy.example.local ceph-deploy
-$ip ceph-mon1.example.local ceph-mon1
-$ip ceph-mon1.example.local ceph-mon2
-$ip ceph-mon1.example.local ceph-mon3
-$ip ceph-mgr1.example.local ceph-mgr1
-$ip ceph-mgr1.example.local ceph-mgr2
-$ip ceph-node1.example.local ceph-node1
-$ip ceph-node2.example.local ceph-node2
-$ip ceph-node2.example.local ceph-node3
+$ip1 ceph-deploy.example.local ceph-deploy
+$ip1 ceph-mon1.example.local ceph-mon1
+$ip2 ceph-mon1.example.local ceph-mon2
+$ip3 ceph-mon1.example.local ceph-mon3
+$ip1 ceph-mgr1.example.local ceph-mgr1
+$ip2 ceph-mgr1.example.local ceph-mgr2
+$ip1 ceph-node1.example.local ceph-node1
+$ip2 ceph-node2.example.local ceph-node2
+$ip3 ceph-node2.example.local ceph-node3
 EOF
 ) >> /etc/hosts
 
@@ -43,7 +46,7 @@ apt install pip -y
 pip install ceph-deploy
 apt-cache madison ceph-deploy
 sudo apt install ceph-deploy -y
-# 如果上面的运行有问题，可以编译安装最新版本的ceph-deploy
+# 如果上面的运行有问题，可以编译安装最新版本的ceph-deploy，但有风险，可能会导致版本和apt的不兼容，最好都用apt安装。
 #mkdir /ceph-deploy-source
 #cd /ceph-deploy-source
 #git clone https://github.com/ceph/ceph-deploy.git
@@ -56,9 +59,8 @@ bash -c "echo 'Asia/Shanghai' > /etc/timezone"
 apt-get install -y ntpdate
 # 如果socket在使用，就
 systemctl stop ntp
+# 或者ntpdate ntp1.aliyun.com
 ntpdate time1.aliyun.com
-# 或者
-ntpdate ntp1.aliyun.com
 
 
 # 生成特定的key
@@ -76,7 +78,9 @@ echo "\n
 su - cephadmin
 # ssh-keygen -m PEM -t rsa -b 4096
 # here use cephadmin password
-ssh-copy-id cephadmin@ceph-server
+ssh-copy-id $ip1
+ssh-copy-id $ip2
+ssh-copy-id $ip3
 "
 
 # 参考资料：极客时间，马哥教育，https://cloud.tencent.com/developer/article/1577952
--- cloud_env_prep/README.md
+++ cloud_env_prep/README.md
@@ -1,4 +1,4 @@
-1. 更新软件，apt update; apt upgrade
-2. 打开安全组，打开所需的端口，一些端口只允许某些特定IP访问。
+1. 更新软件，apt update -y; apt upgrade -y
+2. 打开安全组，打开所需端口，一些端口只允许某些特定IP访问。
 3. 关闭防火墙，apt install firewalld; systemctl stop firewalld;systemctl disable firewalld; 或者只放开某些端口。
 4. 安装vnc需要的软件。
